0e1e40631ae205dc295e417f64a0044f6972bc8e
Carles Pagès <page@cubata.homelinux.net>
2015-03-13 17:07:25 +0100

    Modify project files to try to build client
    It still lacks definitions, but got to building gccg source.

--- android/jni/src/Android.mk
+++ android/jni/src/Android.mk
@@ -2,17 +2,41 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_CPP_FEATURES += exceptions
+
 LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
+	$(LOCAL_PATH)/../SDL2_image \
+	$(LOCAL_PATH)/../SDL2_mixer \
+	$(LOCAL_PATH)/../SDL2_net \
+	$(LOCAL_PATH)/../SDL2_ttf \
+	$(LOCAL_PATH)/../../../src/include
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-	YourSourceHere.c
+	../../../src/client.cpp \
+	../../../src/parser_libcards.cpp \
+	../../../src/parser_libnet.cpp \
+	../../../src/parser.cpp \
+	../../../src/data_filedb.cpp \
+	../../../src/parser_lib.cpp \
+	../../../src/tools.cpp \
+	../../../src/carddata.cpp \
+	../../../src/xml_parser.cpp \
+	../../../src/security.cpp \
+	../../../src/data.cpp \
+	../../../src/localization.cpp \
+	../../../src/driver.cpp \
+	../../../src/game.cpp \
+	../../../src/interpreter.cpp \
+	../../../src/SDL_rotozoom.cpp \
+	../../../src/sdl_driver.cpp \
+	../../../src/game-sdl-version.cpp
 
-LOCAL_SHARED_LIBRARIES := SDL2
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer SDL2_net SDL2_ttf
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
--- android/src/org/libsdl/app/SDLActivity.java
+++ android/src/org/libsdl/app/SDLActivity.java
@@ -65,10 +65,10 @@ public class SDLActivity extends Activity {
     protected String[] getLibraries() {
         return new String[] {
             "SDL2",
-            // "SDL2_image",
-            // "SDL2_mixer",
-            // "SDL2_net",
-            // "SDL2_ttf",
+            "SDL2_image",
+            "SDL2_mixer",
+            "SDL2_net",
+            "SDL2_ttf",
             "main"
         };
     }
