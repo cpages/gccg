b8e683695ddff26d84e45067fa6825c9ae788168
Carles Pagès <page@cubata.homelinux.net>
2015-03-13 17:06:52 +0100

    Modify nix expression to include android build tools

--- default.nix
+++ default.nix
@@ -1,7 +1,9 @@
 with import <nixpkgs> {}; {
   gccgEnv = stdenv.mkDerivation {
     name = "gccg";
-    buildInputs = [ stdenv SDL2 SDL2_image SDL2_net SDL2_ttf SDL2_mixer
-      libjpeg ];
+    buildInputs = [ stdenv
+      SDL2 SDL2_image SDL2_net SDL2_ttf SDL2_mixer libjpeg
+      jdk ant androidenv.androidsdk_4_4 androidenv.androidndk which gnumake
+    ];
   };
 }
